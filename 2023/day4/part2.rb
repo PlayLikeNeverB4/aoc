b303dc8911d331fad7639c8abf5797218ec0b03d
George Marcus <george.marcus922@gmail.com>
2023-12-04 10:22:14 +0200

    2023 day4 part2

---
+++ 2023/day4/part2.rb
@@ -0,0 +1,23 @@
+require 'set'
+$cards = nil
+
+def read_input
+  f = File.open('input.txt', 'r')
+  $cards = f.map{|line| line.split(':').last.split('|').map{|side| side.strip.split.map(&:to_i) }}
+end
+
+def solve
+  winnings = Array.new($cards.size)
+  $cards.each_with_index.reverse_each do |cs, index|
+    matches = cs[0].to_set.intersection(cs[1].to_set).size
+    winnings[index] = 1 + (winnings[index+1..index+matches] || []).sum
+    winnings[index]
+  end
+  winnings.sum
+end
+
+read_input
+
+solution = solve
+
+puts solution
