a72191847c9a38354db67c8cc7b6fff53e3092e8
George Marcus <george.marcus922@gmail.com>
2023-12-15 08:27:43 +0200

    2023 day15 part1

--- 2023/day15/part1.rb
+++ 2023/day15/part1.rb
@@ -1,1 +1,1 @@
-puts File.open('input.txt').readline.strip.split(',').sum{|s| s.chars.reduce(0) { |sum, c| (sum + c.ord) * 17 % 256 } }
+p File.open('in.txt').read.strip.split(',').sum{|s| s.chars.reduce(0) {|sum, c| (sum + c.ord) * 17 % 256 }}
