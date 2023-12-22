c24779eaaad33db65403ef75d13f3a4aeb59f688
George Marcus <george.marcus922@gmail.com>
2023-12-22 17:18:43 +0200

    Some optimizations

--- 2023/day22/part2.rb
+++ 2023/day22/part2.rb
@@ -4,7 +4,7 @@ def read_input
   f = File.open('input.txt', 'r')
   $bricks = f.readlines.map do |line|
     line.strip.split('~').map{|b| b.split(',').map(&:to_i)}.sort_by(&:last)
-  end.sort_by{|b| b[1][2] }
+  end
 end
 
 def overlap_x?(a, b, c, d)
@@ -16,7 +16,6 @@ def overlap_x?(a, b, c, d)
 end
 
 def overlap_xy?(b, nb)
-  !overlap_x?(b[0][2], b[1][2], nb[0][2], nb[1][2]) &&
   overlap_x?(b[0][0], b[1][0], nb[0][0], nb[1][0]) &&
     overlap_x?(b[0][1], b[1][1], nb[0][1], nb[1][1])
 end
@@ -31,20 +30,33 @@ end
 
 def solve
   answer = 0
-  bs = $bricks
+  bs = $bricks.sort_by{|b| b[1][2] }
+  max_z = bs.map{|b| b[1][2]}.max
+  bricks_by_z = Array.new(max_z + 1) { [] }
   edges = []
   bs.each_with_index do |b, bidx|
     next_z = 0
-    bs.first(bidx).each do |nb|
-      next_z = [next_z, nb[1][2]].max if overlap_xy?(b, nb)
+    (0..b[0][2]-1).reverse_each do |z|
+      bricks_by_z[z].each do |idx|
+        nb = bs[idx]
+        if overlap_xy?(b, nb)
+          next_z = nb[1][2]
+          break
+        end
+      end
+      break if next_z > 0
     end
-    bs.first(bidx).each_with_index do |nb, idx|
+    bricks_by_z[next_z].each do |idx|
+      nb = bs[idx]
       edges << [bidx, idx] if nb[1][2] == next_z && overlap_xy?(b, nb)
     end
     fall = b[0][2] - next_z - 1
+    bricks_by_z[b[1][2]].delete(bidx)
     b[0][2] -= fall
     b[1][2] -= fall
+    bricks_by_z[b[1][2]] << bidx
   end
+
   from = {}
   to = {}
   edges.each do |a, b|
